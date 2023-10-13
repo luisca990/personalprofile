ed93a8631be5261206ec277949bca6d2d52345ba
luisca990 <34386325+luisca990@users.noreply.github.com>
2023-10-13 20:43:49 +0000

    "First version"

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,48 @@
  */
 
 import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+const siteProps = {
+  name: "Luis Carlos Romero Cardenas",
+  title: "Software developer & Machine learning engineer",
+  email: "alex@example.com",
+  gitHub: "microsoft",
+  instagram: "microsoft",
+  linkedIn: "satyanadella",
+  medium: "",
+  twitter: "microsoft",
+  youTube: "Code",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
 
 const App = () => {
   return (
-    <>
+    <div id="main">
       <Header />
-      <h3>Built using Codespaces! 🥳</h3>
-      <p>Follow instructions in README to utilize this template for your site or class.</p>
-    </>
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
   );
 };
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,32 @@
 /**
- * Header Component
+ * Header component
  *
- * Sample of a single responsibility component
+ * Top navigation bar for your site. Set to remain visible as the
+ * user scrolls so that they can constantly reach any part of your page.
  */
-
 import React from "react";
 
 const Header = () => {
-  return <h1>Educator React Codespaces JS Template</h1>;
+  return (
+    <div
+      style={{
+        position: "fixed",
+        display: "flex",
+        justifyContent: "center",
+        gap: "2rem",
+        background: "rgba(255,255,255,0.75)",
+        padding: "1rem",
+        top: 0,
+        width: "100%",
+        zIndex: 10,
+      }}
+    >
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#portfolio">Portfolio</a>
+      <a href="#footer">Contact</a>
+    </div>
+  );
 };
 
 export default Header;
